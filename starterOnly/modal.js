dc416386f014c3e2a8784d8b0fb46d9083884dba
beautythieves <96109098+beautythieves@users.noreply.github.com>
2022-10-17 15:11:27 +0200

    form pattern choix ville

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -1,3 +1,21 @@
+/* a faire: implémenter les données du formulaire
+(1) Lier les labels aux entrées dans le HTML en utilisant les attributs "for" et "id" dans le code existant. Corriger le code HTML quand nécessaire.
+(2) Utiliser du JavaScript pur (pas de jQuery) pour terminer le formulaire :
+
+Le formulaire doit être valide quand l'utilisateur clique sur "Submit"
+Les données doivent être saisies correctement :
+(1) Le champ Prénom a un minimum de 2 caractères / n'est pas vide.
+(2) Le champ du nom de famille a un minimum de 2 caractères / n'est pas vide.
+(3) L'adresse électronique est valide.
+(4) Pour le nombre de concours, une valeur numérique est saisie.
+(5) Un bouton radio est sélectionné.FAIT
+(6) La case des conditions générales est cochée, 
+l'autre case est facultative / peut être laissée décochée.
+Conserver les données du formulaire (ne pas effacer le formulaire) lorsqu'il
+ ne passe pas la validation.FAIT
+ */
+//cela signifie quoi?
+
 function editNav() {
   var x = document.getElementById("myTopnav");
   if (x.className === "topnav") {
@@ -7,18 +25,25 @@ function editNav() {
   }
 }
 
+
+
 // DOM Elements
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+console.log(formData, "infos");
 const modalBody = document.querySelector(".modal-body");
 
-const firstName = document.getElementById("first");
+
+const reserve = document.getElementById("reserve");
+console.log(reserve);
+const firstName = document.getElementById("first").value;
+console.log(firstName);
 const lastName = document.getElementById("last");
 const email = document.getElementById("email");
 const birthdate = document.getElementById("birthdate");
-const quantity = document.getElementById("quantity");
-
+const quantity = document.getElementById("quantity");/* nombre de tournois*/
+const city = document.getElementsByName("location");
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
@@ -27,13 +52,106 @@ function launchModal() {
   modalbg.style.display = "block";
 }
 
-// regex statements for filling the form
-const regexFirstName = /^[a-zA-ZéèîïÉÈÎÏ][a-zéèêàçîï]+([-'\s][a-zA-ZéèîïÉÈÎÏ][a-zéèêàçîï]+)?$/;
-const regexName =  /^[a-zA-ZéèîïÉÈÎÏ][a-zéèêàçîï]+([-'\s][a-zA-ZéèîïÉÈÎÏ][a-zéèêàçîï]+)?$/;
-const regexEmail =  /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
-const regexBirtDate = /^(?:0[1-9]|[12]\d|3[01])([\/.-])(?:0[1-9]|1[012])\1(?:19|20)\d\d$/;
-/*pour le nombre de participation au tournoi, pas de regex, juste une valeur*/
+//regex statements for filling the form inutile
+//const regexFirstName = /^[A-Z a-z]{2,25}$/;/*min 2 caracteres*/
+//const regexName =  /^[A-Z a-z]{2,25}$/;/* min 2 caracteres*/
+//const regexEmail =  /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+//const regexBirthDate = /^(?:0[1-9]|[12]\d|3[01])([\/.-])(?:0[1-9]|1[012])\1(?:19|20)\d\d$/;
 
+
+ //récupération des données des saisies des champs du formulaire
+const submit = document.getElementById("reserve");
+
+
+//focntion choix du tournoi
+ function validate() {
+  var formValid = false;
+  var i = 0;
+  while (!formValid && i < location.length) {
+      if (location[i].checked) formValid = true;
+      i++;        
+  }
+  if (!formValid) alert("Veuillez choisir une ville");
+ 
+};
+
+// message d'alerte confirmation de validation du formulaire
+document.getElementById("reserve").addEventListener("submit", function(e) {
+  e.preventDefault();
+
+
+
+  alert("Merci, votre réservation est effective");
+});
+
+/*FONCTION NE MARCHE PAS
+function validateForm()  {
+  
+
+  if( regexFirstName.test(firstName) === false) {
+      alert("Veuillez entrer un prénom");
+  }
+  if(regexName.test(lastName) === false) {
+      alert("Veuillez entrer un nom");
+  }
+  if(regexEmail.test(email) === false) {
+    alert ("Veuillez entrer un email");
+  }
+  if(regexBirthDate.test(birthdate) === false) {
+      alert ("Veuillez entrer une date de naissance");
+  }
+  if (quantity === "") {
+    alert ("Veuillez renseigner un nombre y compris 0");
+  }
+  
+  return true;
+};
+console.log(validateForm)
+*/
 
 /* il faut que tous les champs soient remplis 
 et respectent les regex pour que la validation soit effective*/
+// factoriser les elements
+
+
+/*
+function validate(elmDom) {
+  //regle de validatio
+  switch(elmDom.type){
+    case "text" : 
+    console.log("...")
+      break;
+    default : 
+      break;
+  }
+
+  */
+  //affichage
+
+
+  // text : (content)=>{
+  //   const regex = /^[a-zA-ZéèîïÉÈÎÏ][a-zéèêàçîï]+([-'\s][a-zA-ZéèîïÉÈÎÏ][a-zéèêàçîï]+)?$/;
+  //   console.log(content)
+  //   // regex.test(content)
+  // },
+  // date : (content)=>{},
+  // checkbox: (content)=>{},
+  // radio: (content)=>{},
+  // number: (content)=>{},
+  // email: (content)=>{}
+  /*
+}
+
+function test(elm){
+  console.log("///",elm)
+}
+
+const inputs = document.querySelectorAll("input");
+for (const input of inputs){
+  console.log(input.id,input.type )
+  // validations[input.type](input.value);
+
+
+}
+
+*/
